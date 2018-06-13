7506c84a8382d696d3ab19b354f242018a7c5883
Freddie Johnson Jr <>
2018-06-13 00:12:37 -0700

    Initial UI Design

--- src/AssetsApp/Controller.java
+++ src/AssetsApp/Controller.java
@@ -1,4 +1,31 @@
 package AssetsApp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField textFieldZipFolderLocation;
+
+    @FXML
+    private Button btnZipFolderLocation;
+
+    @FXML
+    private TextField textFieldXMLOutputLocation;
+
+    @FXML
+    private Button btnXMLOutputLocation;
+
+    @FXML
+    private TextArea notificationArea;
+
+    @FXML
+    public void initialize(){
+        textFieldZipFolderLocation.setEditable(false);
+        textFieldXMLOutputLocation.setEditable(false);
+        notificationArea.setEditable(false);
+    }
 }
--- src/AssetsApp/Main.java
+++ src/AssetsApp/Main.java
@@ -11,11 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("AssetsApp.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("ASSETS Zip Extractor (AZE) - BETA");
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
