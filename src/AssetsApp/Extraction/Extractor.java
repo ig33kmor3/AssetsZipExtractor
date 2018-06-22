573f4bf6f2f51221e5745be0040d01f4a8e4963e
Freddie Johnson Jr <>
2018-06-22 13:48:17 -0700

    Fixed Extractor Fileoutputstream

--- src/AssetsApp/Extraction/Extractor.java
+++ src/AssetsApp/Extraction/Extractor.java
@@ -24,16 +24,16 @@ public class Extractor {
                     if(!newDirectory.exists()){
                         newDirectory.mkdirs();
                     }
-                } else if(zipEntry.getName().toLowerCase().endsWith(".zip")){
-//                    new File(newFile.getParent()).mkdirs();
-//                    ZipOutputStream zipOutput = new ZipOutputStream(new FileOutputStream(newFile));
-//                    zipOutput.putNextEntry(zipEntry);
-//                    int len;
-//                    while ((len = zipInput.read(buffer)) > 0){
-//                        zipOutput.write(buffer, 0, len);
-//                    }
-//                    zipOutput.close();
-                    System.out.println("Discovered a Zip File: " + zipEntry.getName());
+//                } else if(zipEntry.getName().toLowerCase().endsWith(".zip")){
+////                    new File(newFile.getParent()).mkdirs();
+////                    ZipOutputStream zipOutput = new ZipOutputStream(new FileOutputStream(newFile));
+////                    zipOutput.putNextEntry(zipEntry);
+////                    int len;
+////                    while ((len = zipInput.read(buffer)) > 0){
+////                        zipOutput.write(buffer, 0, len);
+////                    }
+////                    zipOutput.close();
+//                    System.out.println("Discovered a Zip File: " + zipEntry.getName());
                 } else {
                     new File(newFile.getParent()).mkdirs();
                     FileOutputStream fileOutput = new FileOutputStream(newFile);
