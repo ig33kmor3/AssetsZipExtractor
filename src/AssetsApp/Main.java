f70829adfa9925dbb3bd47a5d48ae2ddb06ce466
Freddie Johnson Jr <>
2018-06-27 17:08:47 -0700

    Updated version number

--- src/AssetsApp/Main.java
+++ src/AssetsApp/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("AssetsApp.fxml"));
-        primaryStage.setTitle("ASSETS Zip Extractor (AZE) - BETA");
+        primaryStage.setTitle("ASSETS Zip Extractor (AZE) - v1.0");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
